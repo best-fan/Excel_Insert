fe7b51edecdcad2438c464b68e52278882c368e0
best_fan <fhtdfff@qq.com>
2017-10-26 16:08:22 +0800

    Initial commit

---
+++ test/test/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(433, 374);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 91);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(506, 245);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(54, 374);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "请选择表格";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(227, 374);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "请选择图片目录";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "请选择添加图片的起始行",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+            this.comboBox1.Location = new System.Drawing.Point(364, 60);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(154, 20);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectionChangeCommitted += new System.EventHandler(this.comboBox1_SelectionChangeCommitted);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "图片保存在第几列？",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+            this.comboBox2.Location = new System.Drawing.Point(197, 60);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(150, 20);
+            this.comboBox2.TabIndex = 5;
+            this.comboBox2.SelectionChangeCommitted += new System.EventHandler(this.comboBox2_SelectionChangeCommitted);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "请选择姓名所在列",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+            this.comboBox3.Location = new System.Drawing.Point(12, 60);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(179, 20);
+            this.comboBox3.TabIndex = 6;
+            this.comboBox3.SelectionChangeCommitted += new System.EventHandler(this.comboBox3_SelectionChangeCommitted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 428);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Excel 批量添加图片";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+    }
+}
+
---
+++ test/test/Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel;
+using Office;
+using System.IO;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        Excel.Application etApp;
+        Excel.Workbook etbook;
+        Excel.Worksheet etsheet;
+        Excel.Range etrange;
+        //SelectName 表格名称   SelectPicPath  图片存放路径  PicRows图片起始行 PicColumns 添加图片的列  Picname 图片名称 
+        //NameColumns 姓名所在列  AllRows 所有行
+        public String SelectName, SelectPicPath, Picname;
+        public int AllRows, PicRows, PicColumns, NameColumns;
+        public int Nowrows;
+        public  Dictionary<int , String> PicNa = new Dictionary<int, String>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// excel 操作；
+        /// </summary>
+        public void Eto()
+        {
+
+            //获取工作表表格
+            etApp = new Excel.Application();
+            etbook = (Excel.Workbook)etApp.Workbooks.Open(SelectName);
+
+            //获取数据区域
+            etsheet = (Excel.Worksheet)etbook.Worksheets.get_Item(1);
+            //获取单元格行数
+            AllRows = etsheet.UsedRange.Rows.Count;
+            //获取单元格列数
+            // columns = etsheet.UsedRange.Columns.Count;
+            //获取数据区域 
+            etrange = (Excel.Range)etsheet.UsedRange;
+
+            double m = etsheet.UsedRange.Height;
+            //MessageBox.Show(m.ToString());
+            // 4. 读取某单元格的数据内容：
+            for (int i = PicRows; i < AllRows; i++)
+            {
+                Picname = ((Excel.Range)etrange.get_Item(i, NameColumns)).Text;
+                PicNa.Add(i,Picname); 
+            }
+
+            //5. 写入某单元格的数据内容：
+
+            // ((Excel.Range)etrange.get_Item(i, j)).Value = strData;
+
+            //6. 关闭文件及相关资源：
+
+            // Get_Close();
+
+        }
+        /// <summary>
+        /// 判段图片是否存在，如果存在进行添加 
+        /// </summary>
+        private void Result()
+        {
+            foreach (KeyValuePair<int, string> kvp in PicNa)
+            {
+                
+           
+            Picname =kvp.Value;
+            string PicturePath = SelectPicPath + "\\" + Picname.Trim() + ".jpg";
+            if (File.Exists(PicturePath))
+            {
+                etsheet = (Excel.Worksheet)etbook.Worksheets.get_Item(1);
+
+                etrange = ((Excel.Range)etrange.get_Item(kvp.Key, PicColumns));
+                Nowrows = kvp.Key;//将写入图片的行数赋值
+                InsertPicture(etrange, etsheet, PicturePath);
+            }
+            else
+            {
+               //和图片插入错误一样 ((Excel.Range)etrange.get_Item(kvp.Key, PicColumns)).Value ="图片不存在";
+                textBox1.AppendText("图片：" + Picname + "不存在 \n");
+                //文件不存在
+            }
+            }
+
+        }
+
+        private void Get_Close()
+        {
+            etbook.Close();
+            etApp.Quit();
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(etrange);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(etsheet);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(etbook);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(etApp);
+        }
+
+
+
+        /// <summary>
+        /// 开始执行
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            PicNa.Clear();//清空集合
+            Boolean bl = false;
+            Fist(ref bl);
+            if (bl == false)
+            {
+                return;
+            }
+            Eto();
+            Result();
+            Get_Close();
+            MessageBox.Show("添加成功", "提示信息");
+        }
+
+        public int test;
+        public float left,top;
+        /// </summary>
+        /// <param name="rng">Excel单元格选中的区域</param>
+        /// <param name="PicturePath">要插入图片的绝对路径。</param>
+        public void InsertPicture(Excel.Range rng, Excel._Worksheet sheet, string PicturePath)
+        {
+            rng.Select();
+            float PicLeft, PicTop, PicWidth, PicHeight;
+           
+          
+            try
+            {
+                //设置行高
+               // etsheet.UsedRange.RowHeight = 200;
+                //设置列宽
+               // etsheet.UsedRange.ColumnWidth = 55;
+                if (test == 0)
+                {
+                    PicLeft = Convert.ToSingle(rng.Left);
+                    left = PicLeft;
+                    
+                   
+                }
+                if (test==1)
+                {
+                    PicTop = Convert.ToSingle(rng.Top);
+                    top = PicTop;
+                }
+
+                PicTop = top * (Nowrows-1);
+                PicWidth = Convert.ToSingle(rng.Width);
+                PicHeight = Convert.ToSingle(rng.Height);
+               // PicLeft = 55;
+              //  PicWidth = 20;
+               // PicHeight = 10;
+
+                //参数含义：
+                //图片路径
+                //是否链接到文件
+                //图片插入时是否随文档一起保存
+                //图片在文档中的坐标位置 坐标
+                //图片显示的宽度和高度
+
+                sheet.Shapes.AddPicture(PicturePath, Office.MsoTriState.msoFalse, Office.MsoTriState.msoTrue, left, PicTop, PicWidth, PicHeight);
+                 test++;
+
+                
+               
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("错误：" + ex.Message);
+            }
+        }
+        /// <summary>
+        /// 选择xls文件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                SelectName = dialog.FileName;
+
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            folder.Description = "选择图片存放目录";
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+
+                SelectPicPath = folder.SelectedPath;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.AppendText("使用说明：\n");
+            textBox1.AppendText("1、选择Excel表格 图片所在目录\n");
+            textBox1.AppendText("2、选择姓名所在列、添加图片列、添加起始行\n");
+            textBox1.AppendText("3、点击开始   等待提示 保存信息\n\n\n\n");
+            textBox1.AppendText("作者：小樊 QQ:393719509");
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+            comboBox3.SelectedIndex = 0;
+        }
+        /// <summary>
+        /// 判断数值是否为空
+        /// </summary>
+        /// <param name="rs"></param>
+        public void Fist(ref  Boolean rs)
+        {
+            Boolean res = false;
+            if (SelectName == string.Empty || SelectName == null)
+            {
+                MessageBox.Show("请选择一个表格");
+            }
+            else if (SelectPicPath == String.Empty || SelectPicPath == null)
+            {
+                MessageBox.Show("请选择图片所在路径");
+            }
+            else if (comboBox1.SelectedIndex == 0)
+            {
+                MessageBox.Show("请选择添加图片的起始行");
+            }
+            else if (comboBox2.SelectedIndex == 0)
+            {
+                MessageBox.Show("请选择图片所在的列数");
+            }
+            else if (comboBox3.SelectedIndex == 0)
+            {
+                MessageBox.Show("请选择姓名所在列数");
+            }
+            else
+            {
+                res = true;
+            }
+            rs = res;
+
+        }
+
+        private void comboBox1_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex != 0)
+            {
+                PicRows = Convert.ToInt32(comboBox1.SelectedItem);
+            }
+        
+        }
+
+        private void comboBox2_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedIndex!=0)
+            {
+                
+           
+            PicColumns = Convert.ToInt32(comboBox2.SelectedItem.ToString());
+            }
+        }
+
+        private void comboBox3_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            if (comboBox3.SelectedIndex!=0)
+            {
+                
+            
+            NameColumns = Convert.ToInt32(comboBox3.SelectedItem.ToString());
+            }
+        }
+
+
+    }
+}
